2e402b644993b090315e9dde13abe08cca798419
Svetlana <november.deluxe@gmail.com>
2024-02-21 10:51:55 +0300

    Second commit

--- GameBoard/board/admin.py
+++ GameBoard/board/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Ad, Category, Feedback
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Ad)
+admin.site.register(Category)
+admin.site.register(Feedback)
+
+class AdAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Ad
+        fields = '__all__'
+
+
+class AdAdmin(admin.ModelAdmin):
+    form=AdAdminForm
--- GameBoard/board/models.py
+++ GameBoard/board/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    personal_email = models.EmailField(max_length=254)
+
+
+class Ad(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ads")
+    categories = models.ForeignKey('Category',null=True,on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = RichTextField(blank=True, null=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+    T = "tanks"
+    H = "heals"
+    D="DD"
+    M="merchants"
+    G="gildmasters"
+    Q="questgivers"
+    S="smiths"
+    L="leatherworkers"
+    P="potioncookers"
+    SM="spellmasters"
+    choices = ((T, "Танки"), (H, "Хилы"), (D, "ДД"), (M, "Торговцы"), (G, "Гилдмастеры"),(Q, "Квестгиверы"),
+               (S, "Кузнецы"), (L, "Кожевники"), (P, "Зельевары"), (SM, "Мастера заклинаний"))
+    choice_field = models.CharField(max_length=255, choices=choices, default="tanks")
+
+
+class Feedback(models.Model):
+    advert = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="feedback")
+    text = models.TextField(max_length=1024)
+    created_at = models.DateTimeField(auto_now_add=True)
+
